551d9fe944687c44bc7e0208d410049c3958a234
Gineth <ginysp@hotmail.com>
2021-04-21 21:56:36 -0600

    Revert "Adrian branch"

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -2,8 +2,9 @@ import React, { Component } from "react";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
-		<p className="text-center">
-			Todos los derechos reservados by <a href="http://www.4geeksacademy.com">4Geeks Academy</a> 2021
+		<p>
+			Made with <i className="fa fa-heart text-danger" /> by{" "}
+			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
 		</p>
 	</footer>
 );
