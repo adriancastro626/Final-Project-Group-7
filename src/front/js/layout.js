4c59f1c4c220782ec330cf83fe10105dbdd54683
SergioCoto <coto.sergio@gmail.com>
2021-04-22 04:34:17 +0000

    Payment view sin funcionalidad

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -5,6 +5,7 @@ import ScrollToTop from "./component/scrollToTop";
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
+import { Payment } from "./pages/payment";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -30,6 +31,9 @@ const Layout = () => {
 						</Route>
 						<Route exact path="/single/:theid">
 							<Single />
+						</Route>
+						<Route exact path="/payment">
+							<Payment />
 						</Route>
 						<Route>
 							<h1>Not found!</h1>
